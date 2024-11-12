92dc369364ab951f7b7f301adfa8b21f5050a243
pushrefresh <rossi@pushrefresh.com>
2024-11-12 00:06:22 -0600

    Update MailchimpSignup.js

--- src/components/MailchimpSignup.js
+++ src/components/MailchimpSignup.js
@@ -12,11 +12,11 @@ const MailchimpEmbedded = () => {
         target="_blank"
       >
         <h3 className="text-xl font-bold mb-4">JOIN OUR MAILING LIST</h3>
-        <div className="flex flex-col md:flex-row max-w-md mx-auto gap-2 md:gap-0">
+        <div className="!flex !flex-col max-w-md mx-auto gap-2 md:!flex-row md:gap-0">
           <input
             type="email"
             name="EMAIL"
-            className="w-full p-2 bg-black border border-zinc-800 rounded md:rounded-l md:rounded-r-none focus:outline-none focus:border-[#D4FF99]"
+            className="!w-full p-2 bg-black border border-zinc-800 !rounded md:!rounded-l md:!rounded-r-none focus:outline-none focus:border-[#D4FF99]"
             id="mce-EMAIL"
             placeholder="Enter your email"
             required
@@ -25,7 +25,7 @@ const MailchimpEmbedded = () => {
             type="submit"
             name="subscribe"
             id="mc-embedded-subscribe"
-            className="w-full md:w-auto px-6 py-2 bg-[#D4FF99] hover:bg-[#bfe589] text-black font-medium rounded md:rounded-r md:rounded-l-none transition duration-300"
+            className="!w-full md:!w-auto px-6 py-2 bg-[#D4FF99] hover:bg-[#bfe589] text-black font-medium !rounded md:!rounded-r md:!rounded-l-none transition duration-300"
           >
             SUBSCRIBE
           </button>
