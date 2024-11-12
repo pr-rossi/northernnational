067bcdfabb26fc1abb116ac719f6eb9915dbab44
Michael Rossi <rossi@pushrefresh.com>
2024-11-11 19:34:49 -0600

    Update ShowCard.js

--- src/components/ShowCard.js
+++ src/components/ShowCard.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 const ShowCard = ({ date, venue, venueUrl, ticketUrl }) => {
   return (
-    <div className="flex flex-col md:flex-row justify-between items-center p-4 bg-gray-900 rounded-lg shadow-lg space-y-4 md:space-y-0">
-      <span className="text-xl font-bold">{date}</span>
+    <div className="flex flex-col md:flex-row justify-between items-center p-4 bg-black rounded-lg shadow-lg space-y-4 md:space-y-0 border border-zinc-900">
+      <span className="text-xl font-bold text-[#D4FF99]">{date}</span>
       <a 
         href={venueUrl}
         target="_blank"
         rel="noopener noreferrer"
-        className="text-gray-300 hover:text-red-500 transition duration-300"
+        className="text-white hover:text-[#D4FF99] transition duration-300"
       >
         {venue}
       </a>
@@ -16,7 +16,7 @@ const ShowCard = ({ date, venue, venueUrl, ticketUrl }) => {
         href={ticketUrl}
         target="_blank"
         rel="noopener noreferrer"
-        className="px-4 py-2 bg-red-500 hover:bg-red-600 rounded transition duration-300 text-white"
+        className="px-4 py-2 bg-[#D4FF99] hover:bg-[#bfe589] text-black font-medium rounded transition duration-300"
       >
         TICKETS
       </a>
