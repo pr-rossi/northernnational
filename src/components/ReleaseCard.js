8b95f9da4a2a291425220e36f0133ec5d4312e06
Michael Rossi <rossi@pushrefresh.com>
2024-11-11 19:34:36 -0600

    Update ReleaseCard.js

--- src/components/ReleaseCard.js
+++ src/components/ReleaseCard.js
@@ -7,9 +7,9 @@ const ReleaseCard = ({ title, description, url }) => {
       href={url}
       target="_blank"
       rel="noopener noreferrer"
-      className="block bg-gray-800 p-6 rounded-lg shadow-lg transform transition duration-300 hover:scale-105 hover:shadow-xl cursor-pointer"
+      className="block bg-zinc-950 p-6 rounded-lg shadow-lg transform transition duration-300 hover:scale-105 hover:shadow-xl cursor-pointer border border-zinc-900 hover:border-[#D4FF99]"
     >
-      <Music className="w-12 h-12 text-orange-500 mb-4" />
+      <Music className="w-12 h-12 text-[#D4FF99] mb-4" />
       <h3 className="text-xl font-bold mb-2 text-white">{title}</h3>
       <p className="text-gray-400">{description}</p>
     </a>
