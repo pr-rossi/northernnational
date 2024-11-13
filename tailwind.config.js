fcc99eb70be21c8f67d47b0e2a15cf86e4385f85
pushrefresh <rossi@pushrefresh.com>
2024-11-13 01:11:57 -0600

    fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,30 @@ export default {
     extend: {
       fontFamily: {
         'thunder': ['Thunder', 'sans-serif'],
+        'sans': ['Aeonik Pro', 'system-ui', 'sans-serif'],
+        'aeonik': ['Aeonik Pro', 'system-ui', 'sans-serif'],
       },
+      fontWeight: {
+        light: 300,
+        normal: 400,
+        medium: 500,
+        bold: 700,
+        black: 900,
+      }
     },
   },
-  plugins: [],
+  plugins: [
+    function({ addBase }) {
+      addBase({
+        'p': { 
+          fontFamily: ['Aeonik Pro', 'system-ui', 'sans-serif'],
+          fontWeight: '400'
+        },
+        'h1, h2, h3, h4, h5, h6': {
+          fontFamily: ['Aeonik Pro', 'system-ui', 'sans-serif'],
+          fontWeight: '700'
+        },
+      })
+    }
+  ],
 }
